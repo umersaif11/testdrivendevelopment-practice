ca95e7c4f64d31f21bf7955182d8fd41cb7b4cf9
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-01 16:37:02 +0500

    Html plugin is not needed as project is JS based testing

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 // webpack.common.js
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -9,11 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-    }),
-  ],
   module: {
     rules: [
       {
