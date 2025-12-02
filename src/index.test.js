650f07aa166ceaf3a725ef06b1611e85a3407ee8
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-02 13:51:03 +0500

    CaesarCipher test: Add the test to handle uppercase too

--- src/index.test.js
+++ src/index.test.js
@@ -84,4 +84,5 @@ test('Calculator divide function', () => {
 test('caesarCipher', () => {
     expect(caesarCipher('xyz', 3)).toBe('abc')
     expect(caesarCipher('abc', 3)).toBe('def')
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr')
 })
